9ae83ab42138bd8e318eabb3dac2a11317535180
ghalib <syedghalib785@gmail.com>
2023-01-05 20:33:57 +0530

    first code

---
+++ src/GitMasterPackage/TestProg1.java
@@ -0,0 +1,12 @@
+package GitMasterPackage;
+
+public class TestProg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is from Indian Guy");
+	}
+
+}
